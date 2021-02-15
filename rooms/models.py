202239812f8a6e472e1311cacf2cc10a7828fa6b
woonsik <dnstlr2933@naver.com>
2021-02-15 22:48:07 +0900

    rooms Application part, 룸에 필요한 특성들을 서버에 추가, 일대일, 일대다 설정

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,97 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from users import models as user_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """ RoomType Obejct Definition """
+
+    pass
+
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["name"]
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Obejct Definition """
+
+    pass
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    pass
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
+    pass
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
+
+class Room(core_models.TimeStampedModel):
+
+    """ Room Model Definition """
+
+    name = models.CharField(max_length=140)  # 이름
+    description = models.TextField()  # 설명
+    country = CountryField()  # 나라
+    city = models.CharField(max_length=80)  # 도시
+    price = models.IntegerField()  # 가격
+    address = models.CharField(max_length=140)  # 주소
+    guests = models.IntegerField()  # 게스트
+    beds = models.IntegerField()  # 침대
+    bedrooms = models.IntegerField()  # 침실
+    baths = models.IntegerField()  # 화장실
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
+
+    def __str__(self):
+        return self.name
--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
-# Register your models here.
-@admin.register(models.User)
-class CustomerUserAdmin(admin.ModelAdmin):
-    pass
+
+@admin.register(models.User)  # admin.site.register(models.User, CustomerUserAdmin)
+class CustomerUserAdmin(UserAdmin):
+
+    """ Customer User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "custom profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
