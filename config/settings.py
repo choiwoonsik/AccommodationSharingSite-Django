120305cef1d80f9d665f0e874e082bf03fd5863c
woonsik <dnstlr2933@naver.com>
2021-05-29 18:38:13 +0900

    방 사진 업로드 오류 수정 4

--- config/settings.py
+++ config/settings.py
@@ -191,9 +191,10 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "woohome-django-web"
+    AWS_BUCKET_ACL = "public-read"
+    AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com"
-    AWS_DEFAULT_ACL = "public-read"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
     sentry_sdk.init(
